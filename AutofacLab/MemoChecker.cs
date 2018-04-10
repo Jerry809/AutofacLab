5fa921d8b4abf823fe910ed18f62994af69ed3df
BESTCO\jerryshih809 <horizon809@gmail.com>
2018-04-10 17:58:51 +0800

    test

---
+++ AutofacLab/MemoChecker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofacLab
+{
+    class Memo
+    {
+        public string Title;
+        public DateTime DueAt;
+    }
+
+    #region 通知備忘事項
+    //產生通知動作的呼叫介面
+    interface IMemoDueNotifier
+    {
+        void MemoIsDue(Memo memo);
+    }
+
+    //實作由TextWriter輸出的通知服務
+    class PrintingNotifier : IMemoDueNotifier
+    {
+        TextWriter _writer;
+        // 傳入TextWriter的建構式
+        public PrintingNotifier(TextWriter writer)
+        {
+            _writer = writer;
+        }
+        // 輸出通知訊息到TextWriter
+        public void MemoIsDue(Memo memo)
+        {
+            _writer.WriteLine("Memo '{0}' is due!", memo.Title);
+        }
+    }
+    #endregion
+
+
+    #region 檢查備忘錄
+    class MemoChecker
+    {
+        IQueryable<Memo> _memos;
+        IMemoDueNotifier _notifier;
+
+        // 建立備忘錄檢查器，顯示到期待辦事項
+        public MemoChecker(IQueryable<Memo> memos, IMemoDueNotifier notifier)
+        {
+            _memos = memos;
+            _notifier = notifier;
+        }
+
+        // 依目前日期找出已到期項目
+        public void CheckNow()
+        {
+            var overdueMemos = _memos.Where(memo => memo.DueAt < DateTime.Now);
+            
+            foreach (var memo in overdueMemos)
+                _notifier.MemoIsDue(memo);
+        }
+    }
+    #endregion
+}
--- AutofacLab/Program.cs
+++ AutofacLab/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,69 @@ namespace AutofacLab
 {
     class Program
     {
+        //在記憶體中建立一個IQueryable<Memo>資料來源
+        static IQueryable<Memo> GenSomeMemos()
+        {
+            IQueryable<Memo> memos = new List<Memo>() {
+                new Memo { Title = "Release Autofac 1.0",
+                           DueAt = new DateTime(2007, 12, 14) },
+                new Memo { Title = "Write CodeProject Article",
+                           DueAt = DateTime.Now },
+                new Memo { Title = "End of The World",
+                           DueAt = new DateTime(2012, 12, 21) }
+            }.AsQueryable();
+            return memos;
+        }
+
+        static ContainerBuilder builder;
+
         static void Main(string[] args)
         {
+            builder = new ContainerBuilder();
+
+            //註冊MemoChecker，建構式的參數也由Container產生
+            builder.Register(c => new MemoChecker(
+               c.Resolve<IQueryable<Memo>>(),
+               c.Resolve<IMemoDueNotifier>())
+               );
+
+            //將PrintingNotifier註冊成IMemoDueNotifier的預設來源
+            builder.Register(c => new PrintingNotifier(
+                c.Resolve<TextWriter>())).As<IMemoDueNotifier>();
+
+            IQueryable<Memo> memos = GenSomeMemos();
+            //直接註冊現有物件是很方便的玩法，在此註冊IQueryable<Memo>的Instance
+            //當需要IQueryable<Memo>時，就會用它
+            builder.RegisterInstance(memos);
+            //Console.Out有實作IDisposable，但不應該由Container來終結
+            //所以要加上ExternallyOwned
+            builder.RegisterInstance(Console.Out).As<TextWriter>().ExternallyOwned();
+
+            InAutofacWay();
+
+            //NoDI();
+            //Console.WriteLine("Done! press any key to exit...");
+            //Console.Read();
+        }
+
+
+        static void NoDI()
+        {
+            //傳統寫法，物件的產生是寫死的
+            MemoChecker chkr = new MemoChecker(GenSomeMemos(), new PrintingNotifier(Console.Out));
+            chkr.CheckNow();
+        }
+
+        static void InAutofacWay()
+        {
+            //使用using包住Container
+            //當程式結束時Autofac會自動處理需要Dispose的物件
+            using (var container = builder.Build())
+            {
+                container.Resolve<MemoChecker>().CheckNow();
+            }
         }
     }
+
+
 }
